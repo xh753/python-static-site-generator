076f674549b6c7f77b3108597c9bd8126534f464
xh753 <129875984+xh753@users.noreply.github.com>
2023-04-04 15:45:32 -0400

    update

--- ssg.py
+++ ssg.py
@@ -3,6 +3,6 @@ from ssg.site import Site
 
 def main(source = "content", dest = "dist"):
     config = {"source": source, "dest": dest}
-    site = Site(**config).build()
+    Site(**config).build()
 
 typer.run(main)
