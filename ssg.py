3e2f23b26f7d88f32e2fcb808ad0c30d2838ecd0
xh753 <129875984+xh753@users.noreply.github.com>
2023-04-04 15:38:55 -0400

    delete print

--- ssg.py
+++ ssg.py
@@ -2,9 +2,7 @@ import typer
 from ssg.site import Site
 
 def main(source = "content", dest = "dist"):
-    #print("Starting")
     config = {"source": source, "dest": dest}
     site = Site(**config).build()
-    #print("Done")
 
 typer.run(main)
--- ssg/site.py
+++ ssg/site.py
@@ -15,6 +15,5 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-                #print("creating path ", path)
 
 
